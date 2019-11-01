9ab3f1b5c4943dc2e4e01ec76aef3d7cefb4c1b7
kjpmj <kjpmj@naver.com>
2019-11-01 18:05:21 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Greetings from './Greetings';
 
-const App: React.FC = () => {
+function App() {
+  const onClick = (name: string) => {
+    console.log(`${name} say hello`);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greetings name="hoho" mark="s" optional="코코코" onClick={onClick} />
     </div>
   );
 }
